006b29c01936d10cf42becba3b6b420d9e968210
karol <karol@KAROL_RG>
2025-11-22 23:16:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace V5ReisContribution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
